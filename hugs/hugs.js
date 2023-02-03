70c0895dea5aa84d58c258975b5563522003a679
gitmustak <124387760+gitmustak@users.noreply.github.com>
2023-02-03 22:53:19 +0530

    Add files via upload

---
+++ hugs/hugs.js
@@ -0,0 +1,10 @@
+const getDiscountBtn = document.querySelector(".get-discount-btn");
+const couponContainer =  document.querySelector(".coupon-container");
+const closeBtn = document.querySelector(".coupon-container .close");
+
+getDiscountBtn.addEventListener("click", () =>{
+    couponContainer.classList.add("active");
+})
+closeBtn.addEventListener("click", () =>{
+    couponContainer.classList.remove("active");
+})
